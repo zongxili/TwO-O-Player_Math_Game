3dcf45899cea580c747f4303486a8f9881e5f0d5
zongxili <zongxiliwork@gmail.com>
2019-10-14 20:21:02 -0700

    refactored code

--- main.rb
+++ main.rb
@@ -1,76 +1,55 @@
 require_relative 'user.rb'
 require_relative 'generate_questions.rb'
-require_relative 'take_turns.rb'
 
-newGame = TakeTurns.new
-
+# initializing players 
 player1 = User.new
 player2 = User.new
 
-# puts "What does #{fir} plus #{sec}"
-
-# puts "the answer is #{total}"
-
-
-def playerOneTurn(player1, player2)
+def playerPerTurn(player1, player2, playerStr)
+  playerThisRound = playerStr
   # initializing the question
   question_generator = Question.new
   fir = question_generator.fir
   sec = question_generator.sec
   total = question_generator.total
   # output the question
-  puts "What does #{fir} plus #{sec}"
+  puts "#{playerThisRound}: What does #{fir} plus #{sec}"
   answer = gets.chomp
   # check the answer and give out the output
   if answer.to_i == total
-    puts "Player 1: YES! You are correct."
+    puts "#{playerThisRound}: YES! You are correct."
   else
-    player1.lose_a_life
-    puts "Player 1: Seriously? No!"
+    if playerThisRound == "Player One"
+      player1.lose_a_life
+    else
+      player2.lose_a_life
+    end
+    puts "#{playerThisRound}: Seriously? No!"
   end
   p1_lives = player1.check_lives
   p2_lives = player2.check_lives
-  # check the result
-  if player1.lives <= 0
-    puts "Player 2 wins with a score of #{p2_lives}"
-    puts "----- GAME OVER -----"
-    puts "Good bye!"
+  if playerThisRound == "Player One"
+    # check the result
+    if player1.lives <= 0
+      puts "Player 2 wins with a score of #{p2_lives}"
+      puts "----- GAME OVER -----"
+      puts "Good bye!"
+    else
+      puts "P1: #{p1_lives} vs P2: #{p2_lives}"
+      puts "----- NEW TURN -----"
+      playerPerTurn(player1, player2, "Player Two")
+    end
   else
-    puts "P1: #{p1_lives} vs P2: #{p2_lives}"
-    puts "----- NEW TURN -----"
-    playerTwoTurn(player1, player2)
+    if player2.lives <= 0
+      puts "Player 1 wins with a score of #{p1_lives}"
+      puts "----- GAME OVER -----"
+      puts "Good bye!"
+    else
+      puts "P1: #{p1_lives} vs P2: #{p2_lives}"
+      puts "----- NEW TURN -----"
+      playerPerTurn(player1, player2, "Player One")
+    end
   end
 end
 
-def playerTwoTurn(player1, player2)
-  # initializing the question
-  question_generator = Question.new
-  fir = question_generator.fir
-  sec = question_generator.sec
-  total = question_generator.total
-  # output the question
-  puts "What does #{fir} plus #{sec}"
-  answer = gets.chomp
-  # check the answer and give out the output
-  if answer.to_i == total
-    puts "Player 2: YES! You are correct."
-  else
-    player2.lose_a_life
-    puts "Player 2: Seriously? No!"
-  end
-  p1_lives = player1.check_lives
-  p2_lives = player2.check_lives
-  # check the result
-  if player2.lives <= 0
-    puts "Player 1 wins with a score of #{p1_lives}"
-    puts "----- GAME OVER -----"
-    puts "Good bye!"
-  else
-    puts "P1: #{p1_lives} vs P2: #{p2_lives}"
-    puts "----- NEW TURN -----"
-    playerOneTurn(player1, player2, "player1")
-  end
-end
-
-
-playerOneTurn(player1, player2)
+playerPerTurn(player1, player2, "Player One")
